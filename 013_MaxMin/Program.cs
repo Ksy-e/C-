b43502350ab71db88e1f2de135c909ddf988d84b
Ksy-e <ksy-e@ya.ru>
2022-10-05 12:49:49 +0500

    MaxMin

---
+++ 013_MaxMin/Program.cs
@@ -0,0 +1,4 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+if(num1>num2) Console.WriteLine($"Число {num1} большее, а число {num2} меньшее");
+else if(num2>num1) Console.WriteLine($"Число {num2} большее, а число {num1} меньшее");
