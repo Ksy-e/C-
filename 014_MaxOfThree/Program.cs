c33956de4399b6f855318b9ace0dbdf1cbaf4fc3
Ksy-e <ksy-e@ya.ru>
2022-10-05 12:58:36 +0500

    Max of three

---
+++ 014_MaxOfThree/Program.cs
@@ -0,0 +1,9 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+int max = 0;
+if (num1>max) max = num1;
+if (num2>max) max = num2;
+if (num3>max) max = num3;
+Console.Write("max = ");
+Console.WriteLine(max);
