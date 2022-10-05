9580d5f16b195124c34890d1a7585c15030a3f43
Ksy-e <ksy-e@ya.ru>
2022-10-05 12:17:07 +0500

    Week

---
+++ 011_Week/Program.cs
@@ -0,0 +1,9 @@
+﻿int num = int.Parse(Console.ReadLine());
+if(num == 1) Console.WriteLine("Понедельник");
+else if(num == 2) Console.WriteLine("Вторник");
+else if(num == 3) Console.WriteLine("Среда");
+else if(num == 4) Console.WriteLine("Четверг");
+else if(num == 5) Console.WriteLine("Пятница");
+else if(num == 6) Console.WriteLine("Суббрта");
+else if(num == 7) Console.WriteLine("Воскресенье");
+else Console.WriteLine("Такого дня нет!");
