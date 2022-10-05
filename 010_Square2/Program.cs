6199956fb355685c1687b42b913782578cf2f749
Ksy-e <ksy-e@ya.ru>
2022-10-05 12:09:09 +0500

    Square2

---
+++ 010_Square2/Program.cs
@@ -0,0 +1,8 @@
+﻿int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+if (number2 * number2 == number1)
+   Console.WriteLine($"число {number1} является квадратом числа {number2}");
+else
+{
+     Console.WriteLine($"число {number1} не является квадратом числа {number2}");
+}
