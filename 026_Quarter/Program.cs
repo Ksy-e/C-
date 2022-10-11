d33869140575507a0eaa1ae54760320c25c5cfd4
Ksy-e <ksy-e@ya.ru>
2022-10-12 02:28:48 +0500

    Quarter2

--- 026_Quarter/Program.cs
+++ 026_Quarter/Program.cs
@@ -1,14 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-void quarter(int x, int y) {
-  if (x>0 && y>0)
-     Console.WriteLine("1 четверть");
-  else if (x>0 && y<0)
-     Console.WriteLine("2 четверть");
-  else if (x<0 && y<0)
-     Console.WriteLine("3 четверть");
-  else 
-     Console.WriteLine("4 четверть");
+//void quarter(int x, int y) {
+ // if (x>0 && y>0)
+  //   Console.WriteLine("1 четверть");
+ // else if (x>0 && y<0)
+ //    Console.WriteLine("2 четверть");
+//  else if (x<0 && y<0)
+  //   Console.WriteLine("3 четверть");
+ // else 
+ //    Console.WriteLine("4 четверть");
+//}
+//int x = int.Parse(Console.ReadLine());
+//int y = int.Parse(Console.ReadLine());
+//quarter(x,y);
+
+// 2 вариант записи
+string quarter2(int x, int y) {
+string name = x>0 && y>0 ? "1 четверть" : x>0 && y<0 ? "2 четверть" : x<0 && y<0 ? "3 четверть" : "4 четверть" ;
+return name;
 }
 int x = int.Parse(Console.ReadLine());
 int y = int.Parse(Console.ReadLine());
-quarter(x,y);
+string otvet = quarter2(x,y);
+Console.WriteLine(otvet);
