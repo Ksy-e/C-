0df5b2f86f7178a170621c24048f794c70ddce43
Ksy-e <ksy-e@ya.ru>
2022-10-13 00:38:42 +0500

    Range

---
+++ 027_Range/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает диапазон
+//возможных координат точек в этой четверти (х,у)
+void diapazon(int a)
+{
+    if(a==1) Console.WriteLine("x>0 и y>0");
+    else if (a==2) Console.WriteLine("x>0 и y<0");
+    else if (a==3) Console.WriteLine("x<0 и y<0");
+    else if (a==4) Console.WriteLine("x<0 и y>0");
+}
+int a = int.Parse(Console.ReadLine());
+diapazon(a);
+
+
