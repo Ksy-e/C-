db63d567d6a4103847026214febf11e7604d49ff
Ksy-e <ksy-e@ya.ru>
2022-10-13 02:41:57 +0500

    Cube

---
+++ 031_Cube/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт 
+//таблицу кубов чисел от 1 до N. 3 -> 1, 8, 27 5 -> 1, 8, 27, 64, 125
+Console.Write("Введите число: ");
+void cube (int num)
+{
+    for (int a = 1; a <= num; a++)
+    {
+    Console.Write($"{a * a * a}");
+    if (a != num) Console.Write(" ");
+    }
+}
+int num = int.Parse(Console.ReadLine());
+cube(num);
