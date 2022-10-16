e616e6e423f389c7961f68c7f6751ded2f028d70
Ksy-e <ksy-e@ya.ru>
2022-10-17 01:30:16 +0500

    Permutations of names

---
+++ 037_PermutationOfNames/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задан список фамилий брокеров товарной биржи из N человек. Обменяйте местами фамилии
+// брокеров: первого и последнего, второго и предпоследнего, третьего от начала и 
+//третьего от конца. Например: Олег, Вася, Камилла, Дора -> Дора, Камилла, Вася, Олег.
+void Vivod(string[] arr)
+{
+    foreach(var i in arr)
+     Console.Write(i+" ");
+     Console.WriteLine();
+}
+string[] Perestanovka(string[] arr)
+{
+    for(int i=0; i<arr.Length/2; i++)
+    {
+        string t = arr[i];
+        arr[i] = arr[arr.Length-i-1];
+        arr[arr.Length-i-1] = t;
+    }
+    return arr;
+}
+int N = int.Parse(Console.ReadLine());
+string[] names = new string[N];
+for (int i=0; i<N; i++)
+{
+names[i] = Console.ReadLine();
+}
+Vivod(names);
+Perestanovka(names);
+Vivod(names);
