a0c96c6aabdee8849a9d5480c3399f716b2e4708
Ksy-e <ksy-e@ya.ru>
2022-10-17 16:04:36 +0500

    Sun of digits

---
+++ 041_SumOfDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11   82 -> 10   9012 -> 12
+
+Console.Write("Введите число: ");
+int num= int.Parse(Console.ReadLine());
+
+void summa(int num)
+{
+int sum = 0;
+for(int i=0; i<num; i++)
+{
+    if (num == 0)
+        break;
+    sum += num % 10;
+    num /= 10;
+}
+Console.WriteLine($"Сумма равна {sum}");
+}
+summa(num);
+
