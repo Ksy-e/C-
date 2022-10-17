a73040ce84ec537d6f2186a28324ab244b9cde40
Ksy-e <ksy-e@ya.ru>
2022-10-17 15:05:46 +0500

    Degree Of Number

---
+++ 040_DegreeOfNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A
+// в натуральную степень B. Сделать в функции, сделать проверку на отрицательность.
+//3, 5 -> 243 (3⁵)   2, 4 -> 16
+
+Console.Write("Введите число 1: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int num2 = int.Parse(Console.ReadLine());
+
+void stepen(int num1, int num2)
+{if(num1 < 0 || num2 < 0) Console.WriteLine("Введено отрицательное число!");
+else for (int i = 0; i <= num2; i++)
+  Console.WriteLine($"{num1}^{num2} = {(long)Math.Pow(num1, i):N0}");
+}
+
+stepen(num1, num2);
