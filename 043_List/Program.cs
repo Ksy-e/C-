f8f8dca4ff40bbc6488133c7bc464adad18b082d
Ksy-e <ksy-e@ya.ru>
2022-10-21 00:54:16 +0500

    List

---
+++ 043_List/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] CreateMas(int n)
+{
+    int[] arr = new int[n];
+    for(int i=0; i<n; i++)
+       arr[i] = new Random().Next(2,10);
+    return arr;   
+}
+void Print(int[] arr)
+{
+    Console.WriteLine(String.Join(" ", arr));
+    Console.Write(" ");
+}
+
+void CreateList(List<int> numb)
+{
+    for(int i=0; i<10; i++)
+    numb.Add(new Random().Next(2,10));
+}
+
+ List<int> numbers = new List<int>();
+ int[] array = new int[10];
+ array = CreateMas(10);
+ Print(array);
+ array.ToList();
+ CreateList(numbers);
+ foreach(var n in numbers)
+ {
+    Console.Write(n + " ");
+ }
+Console.Write(" ");
+numbers.AddRange(array);
+foreach(var n in numbers)
+ {
+    Console.Write(n + " ");
+ }
