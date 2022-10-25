8bf39c982425a24b4f997c5c8578cbd97fe2700f
Ksy-e <ksy-e@ya.ru>
2022-10-25 12:10:02 +0500

    Fibonacci2

---
+++ 056_Fibonacci2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Числа Фибоначии без использования рекурсии
+int n = int.Parse(Console.ReadLine());
+int first = 0;
+int second = 1;
+Console.Write(first+" "+second+" ");
+for(int i=3; i<=n; i++)
+{
+    int next = first+second;
+    Console.Write(next+" ");
+    first=second;
+    second=next;
+}
