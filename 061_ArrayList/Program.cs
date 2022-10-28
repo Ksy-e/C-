2dea1224869c97ea7a0f5368e92d9936ec29f557
Ksy-e <ksy-e@ya.ru>
2022-10-28 12:04:40 +0500

    Array List

---
+++ 061_ArrayList/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте двумерный массив, размером m x n, заполненный случайными вещественными числами
+
+void FillList(List<List<double>> mylist)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        mylist.Add(new List<double>());
+    for (int j = 0; j < 5; j++)
+        mylist[i].Add(Math.Round(new Random().NextDouble() * 5));
+}
+}
+
+void PrintList(List<List<double>> mylist)
+{
+    for (int i = 0; i < mylist.Count; i++)
+    {
+        for (int j = 0; j < mylist.Count; j++)
+            Console.Write(mylist[i][j] + " ");
+    Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+List<List<double>> numbers = new List<List<double>>();
+FillList(numbers);
+PrintList(numbers);
