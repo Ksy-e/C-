1cf0bf1b27a3ac312ccd7cb44ccc540413263827
Ksy-e <ksy-e@ya.ru>
2022-10-28 13:03:24 +0500

    Array List Columns

---
+++ 065_ArrayListColumns/Program.cs
@@ -0,0 +1,43 @@
+﻿// Дан двумерный массив и два числа: i и j. Поменяйте в массиве столбцы с номерами
+// i и j и выведите результат.
+//Программа получает на вход размеры массива n и m, затем элементы массива, затем числа i и j.
+
+void FillList(List<List<double>> mylist)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        mylist.Add(new List<double>());
+        for (int j = 0; j < 5; j++)
+            mylist[i].Add(Math.Round(new Random().NextDouble() * 5));
+    }
+}
+
+void PrintList(List<List<double>> mylist)
+{
+    for (int i = 0; i < mylist.Count; i++)
+    {
+        for (int j = 0; j < mylist.Count; j++)
+            Console.Write(mylist[i][j] + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void Swap(List<List<double>> mylist, int j1, int j2)
+{
+    double temp = 0;
+    for (int i = 0; i < mylist.Count; i++)
+    {
+        for (int j = 0; j < mylist.Count; j++)
+        {
+            temp = mylist[i][j1];
+            mylist[i][j1] = mylist[i][j2];
+            mylist[i][j2] = temp;
+        }
+    }
+}
+List<List<double>> numbers = new List<List<double>>();
+FillList(numbers);
+PrintList(numbers);
+Swap(numbers, 1,3);
+PrintList(numbers);
