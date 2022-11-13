10e349534f3a4f4dd6ce95f4160c5ff79f0e5614
Ksy-e <ksy-e@ya.ru>
2022-11-14 00:34:48 +0500

    081_RecursionCatalog

---
+++ 081_RecursionCatalog/Program.cs
@@ -0,0 +1,23 @@
+﻿// Каталог папок и файлов
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo [] catalogs = catalog.GetDirectories();
+    for(int i =0; i<catalogs.Length;i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent+" ");
+    }
+
+FileInfo[] files = catalog.GetFiles();
+
+for(int i=0; i<files.Length; i++)
+
+{
+    Console.WriteLine($"{indent}{files[i].Name}");
+}
+}
+
+string path =@"/Users/KSY/Desktop/C#/001_Hello";
+CatalogInfo(path);
+
