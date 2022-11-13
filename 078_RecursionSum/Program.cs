ebdb0794eb9c0408ea4c0bc7561014c0837f5fd9
Ksy-e <ksy-e@ya.ru>
2022-11-13 18:16:37 +0500

    078_RecursionSum

---
+++ 078_RecursionSum/Program.cs
@@ -0,0 +1,15 @@
+﻿// Сумма чисел от 1 до n
+int Sum1(int n)
+{
+    int result =0;
+    for (int i =1; i<=n; i++) result+=i;
+    return result;
+}
+
+int Sum2(int n)
+{
+    if (n==0) return 0;
+    else return n+(Sum2(n-1));
+}
+Console.WriteLine(Sum1(10));
+Console.WriteLine(Sum2(10));
