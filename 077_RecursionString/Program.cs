b2bf284117bf9d48a69828b370b6ee7849a07cf5
Ksy-e <ksy-e@ya.ru>
2022-11-13 18:05:55 +0500

    Recursion String

---
+++ 077_RecursionString/Program.cs
@@ -0,0 +1,20 @@
+﻿// Собрать строку с числами от а до б, а<=б
+//Итеративный подход
+string Num1(int a, int b)
+{
+    string result = string.Empty;
+    for(int i=a; i<=b; i++)
+    {
+        result+=$"{i}";
+    }
+    return result;
+}
+
+//Рекурсия
+string Num2(int a, int b)
+{
+    if(a<=b) return $"{a}" + Num2(a+1, b);
+    else return string.Empty;
+}
+Console.WriteLine(Num1(1,10));
+Console.WriteLine(Num2(1,10));
